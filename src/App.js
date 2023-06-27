d1bf674361c384fc3c48844d90ecf0d7470c467d
BrettSantor <b.santor27@gmail.com>
2023-06-27 11:56:28 -0700

    add sprite component and image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import { Stage } from '@pixi/react';
+import { Stage, Sprite } from '@pixi/react';
+import cactus from "./assets/cactus-22155.png"
 
 function App() {
   return (
-   <Stage>
+   <Stage
+   width={300}
+   height={300}
+   options={{
+    backgroundColor: 0x012b30,
+    antialias: true
+   }}
+   >
+    <Sprite image = {cactus}/>
     </Stage>
   );
 }
